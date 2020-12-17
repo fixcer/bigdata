3ca8d564e119fb482ae19ef437f4aea63ce541e3
fixcer <toan@fixcer.me>
2020-12-17 16:00:21 +0700

    result

--- config.sh
+++ config.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
 docker-compose exec configsvr01 sh -c "mongo < /scripts/init-configserver.js"
+# sleep 5
 
 docker-compose exec shard01-a sh -c "mongo < /scripts/init-shard01.js"
+# sleep 5
+
 docker-compose exec shard02-a sh -c "mongo < /scripts/init-shard02.js"
+# sleep 5
+
 docker-compose exec shard03-a sh -c "mongo < /scripts/init-shard03.js"
+# sleep 5
 
 docker-compose exec router01 sh -c "mongo < /scripts/init-router.js"
+echo "\nDone!"
 
 # docker-compose exec router01 mongo --port 27017
 # sh.enableSharding("bigdata")
